fbd28465e710ccffe6e0e6164ade1668185698d8
Rohit Pokhrel <rohitpoks@ip-69-217.wireless.oberlin.edu>
2022-04-10 14:18:52 -0400

    fixed scroll bug

--- scroll.js
+++ scroll.js
@@ -33,7 +33,7 @@ function reveal__horizontal__in() {
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 500;
+    var elementVisible = 150;
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
     } else {
